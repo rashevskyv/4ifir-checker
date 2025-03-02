1e95a0a3ceb91b77856887bd17896d39bdaef7e4
rashevskyv <rashevsky.v@gmail.com>
2025-03-02 21:28:27 +0200

    Added github_api_url and archives_output_dir

--- settings.py
+++ settings.py
@@ -11,12 +11,14 @@ def load_config(file):
         print(f'Error loading config file {file}:', e)
         sys.exit(1)
 
-aio_zip_url = ["https://sintez.io/AIO.zip", "https://sintez.io/AIOB.zip"]
-# aio_zip_url = ["http://192.168.50.168/4ifir/AIO.zip", "http://192.168.50.168/4ifir/AIOB.zip"]
+aio_zip_url = ["https://github.com/rashevskyv/4ifir-checker/releases/latest/download/AIO.zip"]
+# aio_zip_url = ["http://192.168.50.168/4ifir/AIO.zip"]
 aio_zip_path = "aio.zip"
 custom_packs_path = "custom_packs.json"
 file_to_extract = "config/aio-switch-updater/custom_packs.json"
 output_json_path = "custom_packs.json"
+github_api_url = "https://api.github.com/repos/rashevskyv/4ifir-checker/releases/latest"
+archives_output_dir = "archives"  # Змінена директорія виходу
 
 # Load the configuration from external file
 
